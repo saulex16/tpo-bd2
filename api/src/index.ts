eac3620533499368da600a8c15e1fc8cad852964
Elian Paredes <elianparedes.dev@gmail.com>
2023-10-08 17:13:39 -0300

    build: environment variables setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -39,7 +39,6 @@ app.get('/productos', async (req,res) => {
 })
 
 const server = app.listen(3000, () =>
-  console.log(`
-🚀 Server ready at: http://localhost:3000
-⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
+  console.log(`Server ready at: http://localhost:3000`)
 )
+
