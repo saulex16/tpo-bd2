8fe08fd57f7e86e4d15829106c117d97a1c2af91
Nicolás Margenat <nmargenat@itba.edu.ar>
2023-11-01 16:16:55 -0300

    fix: psql queries

--- psql/queries.sql
+++ psql/queries.sql
@@ -1,9 +1,10 @@
--- 1
+-- QUERIES
+-- 1. Obtener el teléfono y el número de cliente del cliente con nombre “Wanda” y apellido“Baker
 SELECT nro_telefono, nro_cliente
 FROM e01_telefono NATURAL JOIN e01_cliente
 WHERE nombre = 'Wanda' AND apellido = 'Baker';
 
--- 2
+-- 2. Seleccionar todos los clientes que tengan registrada al menos una factura
 SELECT *
 FROM e01_cliente c
 WHERE EXISTS (
@@ -12,7 +13,7 @@ WHERE EXISTS (
 		WHERE c.nro_cliente = f.cliente
 );
 
--- 3
+-- 3. Seleccionar todos los clientes que no tengan registrada una factura
 SELECT *
 FROM e01_cliente c
 WHERE NOT EXISTS (
@@ -21,7 +22,7 @@ WHERE NOT EXISTS (
 		WHERE c.nro_cliente = f.cliente
 );
 
--- 4
+-- 4. Seleccionar los productos que han sido facturados al menos 1 vez.
 SELECT *
 FROM e01_producto p
 WHERE EXISTS (
@@ -31,51 +32,51 @@ WHERE EXISTS (
 );
 
 
--- 5
+-- 5. Seleccionar los datos de los clientes junto con sus teléfonos.
 SELECT c.*, t.nro_telefono
 FROM e01_cliente c NATURAL JOIN e01_telefono t;
 
--- 6
-SELECT c.*, count(*)
-FROM e01_cliente c NATURAL JOIN e01_factura f
-GROUP BY c.*;
+-- 6. Devolver todos los clientes, con la cantidad de facturas que tienen registradas (admitir nulos en valores de Clientes).
+SELECT c.nro_cliente, count(nro_factura) as "cantidad_facturas"
+FROM e01_cliente c LEFT OUTER JOIN e01_factura f ON c.nro_cliente = f.nro_cliente
+GROUP BY c.nro_cliente;
 
--- 7
-SELECT f.*
+-- 7. Listar todas las Facturas que hayan sido compradas por el cliente de nombre "Pandora" y apellido "Tate".
+SELECT f.nro_factura
 FROM e01_factura f NATURAL JOIN e01_cliente c
 WHERE nombre = 'Pandora' AND apellido = 'Tate';
 
--- 8
-SELECT *
+-- 8. Listar todas las Facturas que contengan productos de la marca “In Faucibus Inc.”.
+SELECT nro_factura
 FROM e01_factura
 WHERE nro_factura IN (
-			SELECT nro_factura
-			FROM e01_detalle_factura
-			WHERE codigo_producto IN (
-							SELECT codigo_producto
-							FROM e01_producto
-							WHERE marca = 'In Faucibus Inc.'
-			)
+         SELECT nro_factura
+         FROM e01_detalle_factura
+         WHERE codigo_producto IN (
+                     SELECT codigo_producto
+                     FROM e01_producto
+                     WHERE marca = 'In Faucibus Inc.'
+         )
 );
 
--- 9
-SELECT t.nro_telefono, c.*
-FROM e01_cliente c NATURAL JOIN e01_telefono t;
+-- 9.  Mostrar cada teléfono junto con los datos del cliente.
+SELECT t.*
+FROM e01_cliente c RIGHT OUTER JOIN public.e01_telefono t on c.nro_cliente = t.nro_cliente;
 
--- 10
-SELECT c.*, coalesce(sum(f.total_con_iva), 0)
+-- 10.  Mostrar nombre y apellido de cada cliente junto con lo que gastó en total (con IVA incluido).
+SELECT c.nombre, c.apellido, coalesce(sum(f.total_con_iva), 0) as "gasto_total"
 FROM e01_cliente c NATURAL JOIN e01_factura f
-GROUP BY c.*;
+GROUP BY c.nro_cliente;
 
------- VIEWS
--- 1
+-- VISTAS
+-- 1. Se debe realizar una vista que devuelva las facturas ordenadas por fecha.
 CREATE VIEW facturas_por_fecha
 AS
 SELECT *
 FROM e01_facturas
 ORDER BY fecha;
 
--- 2
+-- 2. Se necesita una vista que devuelva todos los productos que aún no han sido facturados.
 CREATE VIEW productos_no_facturados
 AS
 SELECT p.*
